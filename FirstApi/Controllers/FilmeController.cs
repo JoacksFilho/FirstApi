a7c212b36bb16201836a2cd7a30b80b4d2587074
Joacks12 <joackslemos@hotmail.com>
2023-04-04 23:10:20 -0300

    Adicionado Métodos para Alterar, Remover Filmes

--- FirstApi/Controllers/FilmeController.cs
+++ FirstApi/Controllers/FilmeController.cs
@@ -3,6 +3,7 @@ using AutoMapper;
 using FirstAPI.Data;
 using FirstAPI.Data.Dtos;
 using FirstAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirstAPI.Controllers
@@ -34,10 +35,11 @@ namespace FirstAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<Filme> RecuperaFilmes([FromQuery] int skip = 0,
+        public IEnumerable<ReadFilmeDto> RecuperaFilmes([FromQuery] int skip = 0,
             [FromQuery] int take = 50)
         {
-            return _context.Filmes.Skip(skip).Take(take);
+            return _mapper.Map<List<ReadFilmeDto>>(_context.Filmes.Skip
+                (skip).Take(take));
         }
 
         [HttpGet("{id}")]
@@ -45,7 +47,54 @@ namespace FirstAPI.Controllers
         {
             var filme = _context.Filmes.FirstOrDefault(filme => filme.Id == id);
             if (filme == null) return NotFound();
+            var filmeDto = _mapper.Map<ReadFilmeDto>(filme);
             return Ok(filme);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult AtualizaFilme(int id, 
+            [FromBody]UpdateFilmeDto filmeDto)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme => filme.Id == id);
+            if (filme == null) return NotFound();
+            _mapper.Map(filmeDto, filme);
+            _context.SaveChanges();
+
+            return NoContent();           
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult AtualizaFilmeParcial(int id,JsonPatchDocument<UpdateFilmeDto> patch)
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme => filme.Id == id);
+            if (filme == null) return NotFound();
+
+            var filmeParaAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+            patch.ApplyTo(filmeParaAtualizar, ModelState);
+
+            if (!TryValidateModel(filmeParaAtualizar))
+                return ValidationProblem(ModelState);
+          
+            _mapper.Map(filmeParaAtualizar, filme);
+            _context.SaveChanges();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeletaFilme(int id)            
+        {
+            var filme = _context.Filmes.FirstOrDefault(
+                filme => filme.Id == id);
+            if (filme == null) return NotFound();
+
+            _context.Remove(filme);
+            _context.SaveChanges();
+
+            return NoContent();
         }
     }
 }
---
+++ FirstApi/Data/Dtos/ReadFilmeDto.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FirstAPI.Data.Dtos;
+
+public class ReadFilmeDto
+{
+    //DataTransferObject (DTO)
+
+    public string Titulo { get; set; }
+    public string Genero { get; set; }
+    public int Duracao { get; set; }
+
+    public DateTime HoraDaConsulta { get; set; } = DateTime.Now;
+}
